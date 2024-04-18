805337c2f004cd27422a69a472688748a8336ca2
SanskritiAgrawal18 <sanskritiagrawal677@gmail.com>
2024-04-18 20:43:50 +0530

    changing local db to online db

--- db.js
+++ db.js
@@ -6,8 +6,8 @@ const bodyParser = require("body-parser");
 //const mongoURL_local="mongodb://127.0.0.1:27017/emplyoees" // replce emplyees with database name
 //const mongoURL="mongodb+srv://Sanskriti_Agrawal:Sans1234@cluster0.wslna7i.mongodb.net/"
 
-//const mongoURL=process.env.mongoURL; // connection to online mongo db
-const mongoURL=process.env.mongoURL_local; // connection to local mongo db
+const mongoURL=process.env.mongoURL; // connection to online mongo db
+// const mongoURL=process.env.mongoURL_local; // connection to local mongo db
 mongoose.connect(mongoURL,
 	{
 		useNewUrlParser: true, // isko ni likhenge to connection error aa skta h
